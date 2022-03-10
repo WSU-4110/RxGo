e208a94bc7ad72ed0125f530ab0e8b7a203953b8
Asmahan <hussain.asmahan@gmail.com>
2022-03-10 00:44:13 -0500

    SearchInfo.java, design pattern using singleton.

---
+++ app/src/main/java/SearchInfo.java
@@ -0,0 +1,14 @@
+public class SearchInfo {
+    private static SearchInfo Medical;
+    private SearchInfo()
+    {
+
+    }
+    public static SearchInfo getInstance(){
+        if (null == Medical){
+            Medical = new SearchInfo();
+        }
+        return Medical;
+    }
+
+}
