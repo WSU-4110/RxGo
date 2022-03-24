14829640631b272a7654f3a84d748207ba50c6cf
Asmahan <hussain.asmahan@gmail.com>
2022-03-24 17:28:36 -0400

    Drug

--- app/src/main/java/com/example/demorxgo/Info.java
+++ app/src/main/java/com/example/demorxgo/Info.java
@@ -1,14 +1,11 @@
 package com.example.demorxgo;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-
 import com.example.demorxgo.databinding.FragmentInfoBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
