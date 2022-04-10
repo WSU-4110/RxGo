409df9848fed0262f1916532f2900204a50f34ef
Asmahan <hussain.asmahan@gmail.com>
2022-04-10 19:39:11 -0400

    Unit Test for mutator functions for CommonUses of drugs.

---
+++ app/src/test/java/com/example/demorxgo/DrugTest.java
@@ -0,0 +1,16 @@
+﻿package com.example.demorxgo;
+
+import junit.framework.TestCase;
+
+public class DrugTest extends TestCase {
+    String CommonUses;
+
+    public void testSetCommonUses() {
+        CommonUses = "Common Uses";
+        System.out.println(CommonUses);
+    }
+    public void testGetCommonUses() {
+        System.out.println(CommonUses);
+    }
+
+}
