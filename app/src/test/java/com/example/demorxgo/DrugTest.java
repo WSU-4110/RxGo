91ccc0b031c5e792359d7d666243688fdd829d53
Asmahan <hussain.asmahan@gmail.com>
2022-04-10 21:46:51 -0400

    Unit test to use of the get/set function in the drug class.

--- app/src/test/java/com/example/demorxgo/DrugTest.java
+++ app/src/test/java/com/example/demorxgo/DrugTest.java
@@ -1,16 +1,42 @@
 ﻿package com.example.demorxgo;
-
 import junit.framework.TestCase;
 
-public class DrugTest extends TestCase {
-    String CommonUses;
+
+public class DrugTest extends TestCase{
+
 
     public void testSetCommonUses() {
-        CommonUses = "Common Uses";
-        System.out.println(CommonUses);
-    }
-    public void testGetCommonUses() {
-        System.out.println(CommonUses);
+        Drug uses = new Drug ("uses");
+        uses.setCommonUses("Common Uses");
+        assertEquals ( "Common Uses", uses.getCommonUses () );
     }
 
+
+    public void testGetCommonUses() {
+        Drug SideEffects = new Drug ("uses");
+        assertEquals ( "uses",SideEffects.getCommonUses () );
+    }
+
+    public void testSetSideEffects() {
+        Drug SideEffects = new Drug ("SideEffects");
+        SideEffects.setCommonUses("Side Effects");
+        assertEquals ( "Side Effects", SideEffects.getCommonUses () );
+    }
+
+    public void testGetSideEffects() {
+        Drug SideEffects = new Drug ("SideEffects");
+        assertEquals ( "SideEffects",SideEffects.getCommonUses () );
+    }
+
+    public void testSetDrug() {
+        Drug drug = new Drug ("drug");
+        drug.setCommonUses("drug");
+        assertEquals ( "drug", drug.getCommonUses () );
+    }
+
+    public void tesGetDrug() {
+        Drug drug = new Drug ("drug");
+        assertEquals ( "drug",drug.getCommonUses () );
+    }
 }
+
