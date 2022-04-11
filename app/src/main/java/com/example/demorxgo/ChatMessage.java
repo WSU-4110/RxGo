5af4f3cee89f23988a08747934fc9d5604f5298e
ogudino <98849725+ogudino2@users.noreply.github.com>
2022-04-10 22:59:58 -0400

    Testing for message display
    Testing the display for messages

--- app/src/main/java/com/example/demorxgo/ChatMessage.java
+++ app/src/main/java/com/example/demorxgo/ChatMessage.java
@@ -29,12 +29,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.auth.User;
 
 public class ChatMessage extends AppCompatActivity {
 
-    private FirebaseListAdapter<Messages> adapter;
 
+    private FirebaseListAdapter<Messages> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
